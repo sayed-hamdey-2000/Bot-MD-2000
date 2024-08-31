9523b4e791c990c6ae804b448057a9226f33e3d8
Naruto - 2000 <ssawaza30@gmail.com>
2024-08-31 22:10:29 +0300

    Update البوت-كود.js

--- plugins/البوت-كود.js
+++ plugins/البوت-كود.js
@@ -9,7 +9,7 @@ import pino from 'pino';
 import * as ws from 'ws';
 const { CONNECTING } = ws;
 import { Boom } from '@hapi/boom';
-import { makeWASocket } from './simple.js';
+import { makeWASocket } from '../simple.js';
 if (!(global.conns instanceof Array)) global.conns = [];
 let handler = async (m, { conn: _conn, args, usedPrefix, command, isOwner, isROwner }) => {
 if (!global.db.data.settings[_conn.user.jid].jadibotmd && !isROwner) {
