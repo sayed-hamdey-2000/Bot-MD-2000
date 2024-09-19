f166f506137b26f24d953a511aaed2e7364f97ac
Naruto - 2000 <ssawaza30@gmail.com>
2024-09-19 03:35:17 +0300

    ⛊  𝚂𝙰𝚈𝙴𝙳-𝚂𝙷𝙰𝚆𝙰𝚉𝙰

--- plugins/باتش-حرامي.js
+++ plugins/باتش-حرامي.js
@@ -3,7 +3,7 @@ import fs from 'fs';
 import path from 'path';
 import os from 'os';
 
-const linkFilePath = './projectLink.json'; // ملف لتخزين رابط المشروع
+const linkFilePath = './projectLink.json';
 
 const downloadFileContent = async (url) => {
   try {
@@ -35,46 +35,75 @@ const deleteProjectLink = () => {
 const getDefaultBranch = async (repoLink) => {
   const repoPath = repoLink.replace('https://github.com/', '').replace('.git', '');
   const apiUrl = `https://api.github.com/repos/${repoPath}`;
-  
+
   try {
     const response = await axios.get(apiUrl);
-    return response.data.default_branch || 'master'; // العودة إلى 'master' إذا لم يكن هناك فرع افتراضي
+    return response.data.default_branch || 'master';
   } catch (error) {
     console.error('خطأ في جلب معلومات الفرع الافتراضي:', error);
-    return 'master'; // قيمة افتراضية في حال حدوث خطأ
+    return 'master';
   }
 };
 
-const getFileUrl = (repoLink, fileName) => {
+const getFileUrl = (repoLink, fileName, folder) => {
   const repoPath = repoLink.replace('https://github.com/', '').replace('.git', '');
-  const defaultBranch = 'master'; // سيتم تحديثه باستخدام getDefaultBranch
-  return `https://raw.githubusercontent.com/${repoPath}/${defaultBranch}/plugins/${fileName}`;
+  const defaultBranch = 'master';
+   if (!folder) {
+   return `https://raw.githubusercontent.com/${repoPath}/${defaultBranch}/${fileName}`;
+   } else {
+   return `https://raw.githubusercontent.com/${repoPath}/${defaultBranch}/${folder}/${fileName}`;
+   }
 };
 
-const handleFileDownloadAndSend = async (m, conn, fileUrl, fileName) => {
-  let msg = await conn.sendMessage(m.chat, { text: 'جاري تحميل ومعالجة الملف، يرجى الانتظار...' }, { quoted: m });
+
+
+const listFilesInFolder = async (repoLink, folder) => {
+
+  let apiUrl;
+  const repoPath = repoLink.replace('https://github.com/', '').replace('.git', '');
+  const defaultBranch = await getDefaultBranch(repoLink);
+  
+  if (!folder) {
+  apiUrl = `https://api.github.com/repos/${repoPath}/contents/?ref=${defaultBranch}`;
+  } else {
+  apiUrl = `https://api.github.com/repos/${repoPath}/contents/${folder}?ref=${defaultBranch}`;
+  }
 
   try {
-    // تحميل محتوى الملف مباشرة من الرابط
-    const fileContent = await downloadFileContent(fileUrl);
+    const response = await axios.get(apiUrl);
+    return response.data.map(file => file.name);
+  } catch (error) {
+    console.error(`خطأ في جلب قائمة الملفات من مجلد ${folder}:`, error);
+    return [];
+  }
+};
 
-    // إرسال النص الموجود داخل الملف
+const folderExists = async (repoLink, folder) => {
+  const repoPath = repoLink.replace('https://github.com/', '').replace('.git', '');
+  const defaultBranch = await getDefaultBranch(repoLink);
+  const apiUrl = `https://api.github.com/repos/${repoPath}/contents/${folder}?ref=${defaultBranch}`;
+
+  try {
+    const response = await axios.get(apiUrl);
+    return response.status === 200;
+  } catch (error) {
+    return false;
+  }
+};
+
+const handleFileDownloadAndSend = async (m, conn, fileUrl, fileName, imgUrl) => {
+  let msg = 'جاري تحميل ومعالجة الملف، يرجى الانتظار...';
+  await conn.sendMessage(m.chat, { image: { url: imgUrl }, caption: msg, mentions: [m.sender] }, { quoted: m });
+
+  try {
+    const fileContent = await downloadFileContent(fileUrl);
     await conn.sendMessage(m.chat, { text: fileContent }, { quoted: m });
 
-    // إنشاء مسار مؤقت للملف
     const tempFilePath = path.join(os.tmpdir(), fileName);
-
-    // حفظ محتوى الملف في ملف مؤقت
     fs.writeFileSync(tempFilePath, fileContent);
 
-    // إرسال الملف كمرفق باستخدام sendFile
     await conn.sendFile(m.chat, tempFilePath, fileName, '', m, { mimetype: 'application/javascript' });
-
-    // حذف الملف المؤقت بعد الإرسال
     fs.unlinkSync(tempFilePath);
-
-    // تحديث رسالة الانتظار
-    await conn.sendMessage(m.chat, { text: 'تم تحميل الملف وإرساله بنجاح!' }, { quoted: m });
 
   } catch (error) {
     console.error('خطأ في تحميل أو معالجة الملف:', error);
@@ -82,109 +111,127 @@ const handleFileDownloadAndSend = async (m, conn, fileUrl, fileName) => {
   }
 };
 
-const listFilesInRepo = async (repoLink) => {
-  const repoPath = repoLink.replace('https://github.com/', '').replace('.git', '');
-  const defaultBranch = await getDefaultBranch(repoLink);
-  const apiUrl = `https://api.github.com/repos/${repoPath}/contents/plugins?ref=${defaultBranch}`;
-
-  try {
-    const response = await axios.get(apiUrl);
-    return response.data.map(file => file.name); // الإبقاء على امتداد .js
-  } catch (error) {
-    console.error('خطأ في جلب قائمة الملفات:', error);
-    return [];
-  }
-};
-
 const formatFileList = (files) => {
   if (files.length === 0) {
     return 'لم يتم العثور على ملفات.';
   }
-  
-  const header = `╭──────────────────╮\n│ لم يتم ادخال اسم ملف !!\n│ عدد الملفات المتاحة: ${files.length}\n╰──────────────────╯`;
+  const header = `╭──────────────────╮\n│ قائمة الملفات المتاحة.\n│ عدد الملفات المتاحة: ${files.length}\n╰──────────────────╯`;
   const fileList = files.map((file, index) => `│ [${index + 1}] ${file}`).join('\n');
   return `${header}\n\n╭──────────────────╮\n${fileList}\n╰──────────────────╯`;
 };
 
-// تحقق من ما إذا كان الرابط يشير إلى مستودع عام
 const isRepositoryPublic = async (repoLink) => {
   const repoPath = repoLink.replace('https://github.com/', '').replace('.git', '');
   const apiUrl = `https://api.github.com/repos/${repoPath}`;
-  
+
   try {
     const response = await axios.get(apiUrl);
-    return !response.data.private; // إذا كان private == false، فهذا يعني أنه عام
+    return !response.data.private;
   } catch (error) {
     console.error('خطأ في جلب معلومات المستودع:', error);
     return false;
   }
 };
 
-const handler = async (m, { conn, isROwner, usedPrefix, command, text }) => {
+const handler = async (m, { conn, usedPrefix, command, text }) => {
+  const projectLink = getProjectLink();
+
   if (command === 'res') {
-    // أمر لتعيين رابط المشروع
     if (!text) return m.reply('يرجى تقديم رابط المشروع.');
     if (!text.startsWith('https://github.com/') || !text.endsWith('.git')) {
       return m.reply('الرابط يجب أن يكون رابط GitHub ينتهي بـ ".git".');
     }
 
     const isPublic = await isRepositoryPublic(text);
-    if (!isPublic) {
-      return m.reply('الرابط يشير إلى مستودع خاص. يرجى تقديم رابط لمستودع عام.');
-    }
+    if (!isPublic) return m.reply('الرابط يشير إلى مستودع خاص.');
 
     saveProjectLink(text);
-    m.reply('تم حفظ رابط المشروع بنجاح.');
+    return m.reply('تم حفظ رابط المشروع بنجاح.');
 
   } else if (command === 'rev') {
-    // أمر لعرض الملفات من الرابط المخزن
-    const projectLink = getProjectLink();
     if (!projectLink) {
-      return m.reply(
-        `لم يتم تعيين رابط مشروع بعد. يرجى تعيين الرابط باستخدام الأمر ${usedPrefix}res <رابط>\n` +
-        `مثال: ${usedPrefix}res https://github.com/username/repo.git`
-      );
+      return m.reply(`لم يتم تعيين رابط مشروع بعد. يرجى تعيين الرابط باستخدام الأمر ${usedPrefix}res <رابط>.`);
     }
 
-    const files = await listFilesInRepo(projectLink);
+    const branchroject = await getDefaultBranch(projectLink);
+    const files = await listFilesInFolder(projectLink, 'plugins');
+    const imgfiles = `https://image.thum.io/get/fullpage/${projectLink.replace('.git', '')}/tree/${branchroject}/plugins/`;
 
     if (!text) {
-      // عرض قائمة الملفات إذا لم يتم إدخال اسم الملف
-      return m.reply(formatFileList(files));
-    } else {
-      // عرض محتوى الملف إذا تم إدخال اسم الملف
-      const fileNameWithExt = `${text}.js`; // التأكد من أن اسم الملف ينتهي بـ .js
-      if (!files.includes(fileNameWithExt)) {
-        return m.reply(`╭──────────────────╮\n│ الملف "${fileNameWithExt}" غير موجود.\n│ القائمة المتاحة هي:\n╰──────────────────╯\n\n\n╭──────────────────╮\n${formatFileList(files)}\n╰──────────────────╯`);
-      }
-
-      const fileUrl = getFileUrl(projectLink, fileNameWithExt);
-        
-      await handleFileDownloadAndSend(m, conn, fileUrl, fileNameWithExt);
+      return await conn.sendMessage(m.chat, { image: { url: imgfiles }, caption: formatFileList(files), mentions: [m.sender] }, { quoted: m });
     }
+
+    const fileNameWithExt = `${text}.js`;
+    const fileUrl = getFileUrl(projectLink, fileNameWithExt, 'plugins');
+
+    if (!files.includes(fileNameWithExt)) {
+      return m.reply(`الملف "${fileNameWithExt}" غير موجود.`);
+    }
+
+    await handleFileDownloadAndSend(m, conn, fileUrl, fileNameWithExt, imgfiles);
 
   } else if (command === 'red') {
-    // أمر لحذف الرابط المخزن
-    const projectLink = getProjectLink();
-    if (projectLink) {
-      deleteProjectLink();
-      m.reply('تم حذف رابط المشروع.');
-    } else {
-      m.reply(
-        `لم يتم تعيين رابط مشروع بعد. يرجى تعيين الرابط باستخدام الأمر ${usedPrefix}res <رابط>\n` +
-        `مثال: ${usedPrefix}res https://github.com/username/repo.git`
-      );
+    if (!projectLink) {
+      return m.reply(`لم يتم تعيين رابط مشروع بعد.`);
     }
+
+    deleteProjectLink();
+    return m.reply('تم حذف رابط المشروع.');
+
+  } else if (command === 'reb') {
+    if (!projectLink) {
+      return m.reply(`لم يتم تعيين رابط مشروع بعد.`);
+    }
+
+    const libFolder = 'lib';
+    const srcLibrariesFolder = 'src/libraries';
+    const folder = await folderExists(projectLink, libFolder) ? libFolder : srcLibrariesFolder;
+
+    const branchroject = await getDefaultBranch(projectLink);
+    const files = await listFilesInFolder(projectLink, folder);
+    const imgfiles = `https://image.thum.io/get/fullpage/${projectLink.replace('.git', '')}/tree/${branchroject}/${folder}/`;
+
+    if (!text) {
+      return await conn.sendMessage(m.chat, { image: { url: imgfiles }, caption: formatFileList(files), mentions: [m.sender] }, { quoted: m });
+    }
+
+    const fileNameWithExt = `${text}.js`;
+    const fileUrl = getFileUrl(projectLink, fileNameWithExt, folder);
+
+    if (!files.includes(fileNameWithExt)) {
+      return m.reply(`الملف "${fileNameWithExt}" غير موجود.`);
+    }
+
+    await handleFileDownloadAndSend(m, conn, fileUrl, fileNameWithExt, imgfiles);
+
+  } else if (command === 'ref') {
+    if (!projectLink) {
+      return m.reply(`لم يتم تعيين رابط مشروع بعد.`);
+    }
+
+    const branchroject = await getDefaultBranch(projectLink);
+    const files = await listFilesInFolder(projectLink, '');
+    const imgfiles = `https://image.thum.io/get/fullpage/${projectLink.replace('.git', '')}/tree/${branchroject}`;
+
+
+    if (!text) {
+      return await conn.sendMessage(m.chat, { image: { url: imgfiles }, caption: formatFileList(files), mentions: [m.sender] }, { quoted: m });
+    }
+
+    const fileNameWithExt = `${text}.js`;
+    const fileUrl = getFileUrl(projectLink, fileNameWithExt, '');
+
+    if (!files.includes(fileNameWithExt)) {
+      return m.reply(`الملف "${fileNameWithExt}" غير موجود.`);
+    }
+
+    await handleFileDownloadAndSend(m, conn, fileUrl, fileNameWithExt, imgfiles);
   }
 };
 
-handler.help = [
-  'res *<رابط>* - لتعيين رابط المشروع',
-  'rev *<اسم ملف>* - لعرض محتوى ملف من رابط المشروع (أو عرض قائمة الملفات المتاحة إذا لم يتم تحديد اسم الملف)',
-  'red - لحذف رابط المشروع'
-];
+handler.help = ['res <رابط>', 'rev <اسم ملف>', 'red', 'reb', 'ref'];
 handler.tags = ['owner'];
-handler.command = /^(res|rev|red)$/i;
+handler.command = ['res', 'rev', 'red', 'reb', 'ref'];
 handler.rowner = true;
 
 export default handler;
